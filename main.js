cb318af98e25dbd22feac6cbe80c8928e5ba5aad
frrannkko <121842945+franco-agatiello@users.noreply.github.com>
2025-08-17 17:12:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -171,10 +171,7 @@ window.mostrarTrayectoria = function(index) {
     if (mapaTrayectoria) { mapaTrayectoria.remove(); mapaTrayectoria = null; }
     mapaTrayectoria = L.map('mapTrayectoria').setView([d.lugar_caida.lat, d.lugar_caida.lon], 3);
    var CartoDB_PositronNoLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 20
-});
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapa);
 
     const satrec = satellite.twoline2satrec(d.tle1, d.tle2);
 
